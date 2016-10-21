72f45813dcb3c64c83196c2e0fa3a11d39b67a7a
Andrew Rembrandt <andrew@rembrandt.me.uk>
2016-10-21 21:40:04 +0100

    Interim multi-project

---
+++ crag-mapper-client/src/main/scala/org/github/andrewrembrandt/cragmapperclient/Main.scala
@@ -0,0 +1,12 @@
+package org.github.andrewrembrandt.cragmapperclient
+
+import scala.scalajs.js.JSApp
+
+/**
+  * Created by Andrew on 21/10/2016.
+  */
+object Main extends JSApp{
+  def main(): Unit = {
+    println("HW")
+  }
+}
