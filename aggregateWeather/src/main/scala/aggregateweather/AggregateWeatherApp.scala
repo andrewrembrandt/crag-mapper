48986a0538c9ba70729c5e2f2df56d846ba9138e
Andrew Rembrandt <andrew@rembrandt.me.uk>
2017-09-04 13:03:44 +0100

    Partial kafka

--- aggregateWeather/src/main/scala/aggregateweather/AggregateWeatherApp.scala
+++ aggregateWeather/src/main/scala/aggregateweather/AggregateWeatherApp.scala
@@ -1,9 +1,25 @@
 package aggregateweather
 
 import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
+import akka.serialization.ByteArraySerializer
+import akka.stream.scaladsl.Source
+import com.fasterxml.jackson.databind.ser.std.StringSerializer
+import org.apache.kafka.clients.producer.ProducerRecord
 
 object AggregateWeatherApp extends App {
   implicit val system = ActorSystem("aggregateweather")
 
-  println("Hello World")
+  println("Starting...")
+
+  val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+    .withBootstrapServers("localhost:9092")
+
+  val done = Source(1 to 100)
+    .map(_.toString)
+    .map { elem =>
+      new ProducerRecord[Array[Byte], String]("topic1", elem)
+    }
+    .runWith(Producer.plainSink(producerSettings))
 }
